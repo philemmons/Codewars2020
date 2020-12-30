c22f35b4d58f5472453c2d1cad3c4340b9084b97
Phillip Emmons <31429632+philemmons@users.noreply.github.com>
2020-12-29 20:00:03 -0800

    Create MeetingAlt.java

---
+++ Meeting/MeetingAlt.java
@@ -0,0 +1,16 @@
+//Solution provided by krovosisya, denys12 - excellent job!
+
+import java.util.Arrays;
+
+class MeetingAlt {   
+  public static String meeting(String s) {
+    String[] friends = s.toUpperCase().split(";");
+        for (int i = 0; i < friends.length; i++){
+            int index = friends[i].indexOf(":");
+            friends[i] = "(" + friends[i].substring(index+1) + ", " + friends[i].substring(0,index) + ")";
+        }
+        Arrays.sort(friends);
+        
+        return String.join("", friends);
+    }
+}
