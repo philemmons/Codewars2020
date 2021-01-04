32c05b4b8131c2e7a47c8a3e63d90aeff474dd6b
Phillip Emmons <31429632+philemmons@users.noreply.github.com>
2021-01-03 18:21:38 -0800

    Update DirReduction.java
    simplified regular expression

--- Directions Reduction/DirReduction.java
+++ Directions Reduction/DirReduction.java
@@ -4,12 +4,7 @@ public class DirReduction {
       for(String s: arr) nesw+= s+ ",";
       
       while(nesw.contains("NORTH,SOUTH,") || nesw.contains("SOUTH,NORTH,") || nesw.contains("EAST,WEST,") || nesw.contains("WEST,EAST,")){
-        
-            if(nesw.contains("NORTH,SOUTH,")) nesw= nesw.replaceAll("NORTH,SOUTH,", "");
-            if(nesw.contains("SOUTH,NORTH,")) nesw= nesw.replaceAll("SOUTH,NORTH,", "");
-        
-            if(nesw.contains("EAST,WEST,")) nesw= nesw.replaceAll("EAST,WEST,", "");
-            if(nesw.contains("WEST,EAST,")) nesw= nesw.replaceAll("WEST,EAST,", "");
+                nesw= nesw.replaceAll("NORTH,SOUTH,|SOUTH,NORTH,|EAST,WEST,|WEST,EAST,", "");
       }
       if(nesw.length()>0) 
           arr = nesw.split(",");
