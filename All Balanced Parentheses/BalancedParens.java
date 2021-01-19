60fd27e8bc310a3e1cddb65a45a37ff455308c48
Phillip Emmons <31429632+philemmons@users.noreply.github.com>
2021-01-18 20:29:56 -0800

    Create BalancedParens.java

---
+++ All Balanced Parentheses/BalancedParens.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class BalancedParens {
+	
+	public static String join(String ans, int i) {
+		  return ans.substring(0, i)+ "()"+ ans.substring(i);
+	}
+
+	public static List <String> balancedParens (int n) {
+		  System.out.println("n-pair of parenthesis: "+ n);
+		  if(n== 0) return Arrays.asList("");
+		   
+	    Set<String> lhsCurr = new LinkedHashSet<>();
+	    Set<String> lhsNext = new LinkedHashSet<>();
+	    
+	    lhsCurr.add("");
+	    
+	    for(int i= 0; i< n; i++) {
+		   for(String s: lhsCurr) {
+			   for(int k= 0; k<= s.length(); k++) {
+				   lhsNext.add(join(s, k));
+			   }
+		   }
+		   lhsCurr.clear();
+		   lhsCurr.addAll(lhsNext);
+		   lhsNext.clear(); 
+	    }
+	   
+	   List<String> result = new ArrayList<>(lhsCurr.size());
+	   for(String s: lhsCurr)
+		   result.add(s);
+	    
+	   return result;
+    }
+	
+	public static void main(String[] args) {
+          System.out.println(balancedParens(0));
+          //new String[] {""}
+          System.out.println(balancedParens(1));
+          //new String[] {"()"}
+          System.out.println(balancedParens(2));
+          //new String[] {"(())","()()"}
+          System.out.println(balancedParens(3));
+          //new String[] {"((()))","(()())","(())()","()(())","()()()"}
+          System.out.println(balancedParens(4));
+          //new String[] {"(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"}
+
+	}
