2949715f45dc225822ae6027a52bb45f249ba477
Phillip Emmons <31429632+philemmons@users.noreply.github.com>
2021-02-22 17:52:57 -0800

    Create ConverterTwo.java

---
+++ Incomplete/Decimal to any Rational or Irrational Base Converter/ConverterTwo.java
@@ -0,0 +1,35 @@
+// Author: jberg
+
+public class ConverterTwo {
+
+	private static final String DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static String converter(double n, int decimals, double base) {
+	    if (n == 0) return "0";
+
+	    boolean isNeg = n < 0;
+	    n = Math.abs(n);
+
+	    StringBuilder sb = new StringBuilder();
+	    for (int exponent = (int) (Math.log(n) / Math.log(base)); exponent > -decimals - 1; exponent--) {
+	        if (exponent == -1) {
+	            sb.append(".");
+	        }
+	        sb.append(DIGITS.charAt((int) (n / Math.pow(base, exponent))));
+	        n %= Math.pow(base, exponent);
+	    }
+	    return (isNeg ? "-" : "") + sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(ConverterTwo.converter(13, 0, Math.PI));
+//		103
+		
+		System.out.println(ConverterTwo.converter(17, 0, 2.5));
+//		212
+		
+		System.out.println(ConverterTwo.converter(123456789, 0, 16));
+//		75BCD15
+	}
+
+}
